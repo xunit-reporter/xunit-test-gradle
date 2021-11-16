64dc38f5e28e80eb4febf030e3c993244054ea11
Jeff MAURY <jmaury@redhat.com>
2021-11-16 08:46:41 +0100

    Initial commit
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

---
+++ src/test/java/xunitreporter/io/GreetingResourceTest.java
@@ -0,0 +1,49 @@
+package xunitreporter.io;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class GreetingResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("Hello RESTEasy"));
+    }
+
+    @Test
+    public void testHelloEndpoint1() {
+        given()
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("Hello RESTEasy"));
+    }
+
+    @Disabled
+    @Test
+    public void testHelloEndpointSkipped() {
+        given()
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("Hello RESTEasy"));
+    }
+
+    @Test
+    public void testHelloEndpointFailed() {
+        given()
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("Hello"));
+    }
+}
